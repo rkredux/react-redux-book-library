66f69922681edad9b3e976161596552a87fb1099
Rahul <rkredux@gmail.com>
2017-09-01 05:23:53 +0530

    Create Redux App

---
+++ src/actions/index.js
@@ -0,0 +1,8 @@
+export function selectBook(book){
+	// console.log(`${book.title} was selected by the user`);
+	return {
+		type: "BOOK_SELECTED", 
+		payload: book
+	}; 
+}
+
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default function BooksReducer(){
+	return [
+	{title: "Secrets of JavaScript Ninja"}, 
+	{title: "JavaScript: The Good Parts"}, 
+	{title: "You don't know JavaScript"}, 
+	{title: "JS: The untold store"}
+	]
+}
+
+
